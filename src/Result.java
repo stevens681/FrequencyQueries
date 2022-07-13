d0e48bcf565a933741cc401450611bc2db65fcae
fernandorosa <stevensfrr@gmail.com>
2022-07-12 21:47:06 -0400

    Without comments

---
+++ src/Result.java
@@ -0,0 +1,98 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Result {
+    // Complete the freqQuery function below.
+    static List<Integer> freqQuery(List<List<Integer>> queries) {
+        List<Integer> res = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+
+        for (List<Integer> query : queries) {
+            int q = query.get(0), elements = query.get(1);
+            boolean flag = map.containsKey(elements);
+            switch (q) {
+                case 1: {
+                    if (!flag) {
+                        map.put(elements, 1);
+                    }
+                    else {
+                        map.put(elements, map.get(elements) + 1);
+
+                    }
+                    break;
+                }
+                case 2:
+                    if (flag) {
+                        map.put(elements, map.get(elements) - 1);
+                    }
+                    break;
+                case 3:
+                    if (map.containsValue(elements))
+                        res.add(1);
+                    else
+                        res.add(0);
+                    break;
+            }
+
+        }
+        return res;
+
+    }
+    public static void main(String[] args) {
+//        String list = """
+//                1 5
+//                1 6
+//                3 2
+//                1 10
+//                1 10
+//                1 6
+//                2 5
+//                3 2""";
+        StringBuilder list = new StringBuilder();
+//        String list = """
+//                1 3
+//                2 3
+//                3 2
+//                1 4
+//                1 5
+//                1 5
+//                1 4
+//                3 2
+//                2 4
+//                3 2""";
+        List<List<Integer>> arr = new ArrayList<>();
+
+//        String[] split = list.toString().split("\n");
+        try {
+            File myObj = new File("/Users/fernandorosa/IdeaProjects/Frequency Queries/src/Array");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                list.append(myReader.nextInt()).append("\n");
+
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        
+
+
+            String[] space = list.toString().split("\n");
+
+            List<Integer> hold = new ArrayList<>();
+            for (String s : space) {
+                hold.add(Integer.parseInt(s));
+
+            }
+            if(!hold.isEmpty())arr.add(hold);
+
+
+        System.out.println(arr);
+    }
+}
